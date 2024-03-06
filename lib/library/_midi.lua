0dff19cae1abaa49e1f60b15c68bcec29098f7c4
D---R--- <160875025+melocoi@users.noreply.github.com>
2024-03-05 21:18:03 -0500

    Update _midi.lua
    - added a length multiplier input for extra long notes

--- lib/library/_midi.lua
+++ lib/library/_midi.lua
@@ -2,13 +2,15 @@ local midi_out = function(self, x, y)
   self.y = y
   self.x = x
   self.name = "midi"
-  self.ports = { {1, 0, "in-port"}, {2, 0, "in-octave"}, {3, 0, "in-note"}, {4, 0, "in-velocity"}, {5, 0, "in-length"} }
+  self.ports = { {1, 0, "in-port"}, {2, 0, "in-octave"}, {3, 0, "in-note"}, {4, 0, "in-velocity"}, {5, 0, "in-length"}, {6, 0, "in-mult"} }
   self:spawn(self.ports)
 
   local channel = util.clamp(self:listen(self.x + 1, self.y) or 0, 0, 16)
   local octave = util.clamp(self:listen(self.x + 2, self.y) or 4, 0, 8)
   local vel = util.clamp(self:listen(self.x + 4, self.y) or 10, 0, 16)
+  local multi = self:listen(self.x + 6, self.y) or 1
   local length = self:listen(self.x + 5, self.y) or 1
+  local rlength = length*multi
   local note = self:glyph_at(self.x + 3, self.y) or "C"
   local transposed = self:transpose(note, octave)
   local n, oct, velocity = transposed[1], transposed[4], math.floor((vel / 16 ) * 127)
@@ -17,7 +19,8 @@ local midi_out = function(self, x, y)
 
   if self:neighbor(self.x, self.y, "*") then
     self.midi_out_device:note_on(n, velocity, channel)
-    self:add_note(channel, n, length, false)
+    self:add_note(channel, n, rlength, false)
+    
   end
 end
 
